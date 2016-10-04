fa7a4bdd909e21b4faa668d5e34b31f70db83d6e
Luke Keller <lukeekeller@gmail.com>
2016-10-04 16:30:41 -0400

    Create DocsComponent
    Creates a DocsComponent for the Policy Guide content to live.
    
    * Create DocsComponent
    * Remove `/policy-guide` route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { DocsComponent } from './components/policy-guide/docs';
 import { HomeComponent } from './components/home';
 import { PolicyGuideComponent } from './components/policy-guide';
 
@@ -7,5 +8,11 @@ import { DataResolver } from './app.resolver';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'policy-guide', component: PolicyGuideComponent }
+  {
+    path: 'policy-guide',
+    component: PolicyGuideComponent,
+    children: [
+      { path: 'docs',  component: DocsComponent }
+    ]
+  }
 ];
