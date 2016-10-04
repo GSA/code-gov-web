1cb0fe3beac0764b5bceb0067f371e78f00c7817
Luke Keller <lukeekeller@gmail.com>
2016-10-04 15:10:34 -0400

    Create PolicyGuideComponent
    Create PolicyGuideComponent for including navigation items to different sections of the PolicyGuide.
    
    * Create PolicyGuideComponent with placeholder links to Docs, Read the Policy, and FAQs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home';
+import { PolicyGuideComponent } from './components/policy-guide';
 
 import { DataResolver } from './app.resolver';
 
 
 export const ROUTES: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'policy-guide', component: PolicyGuideComponent }
 ];
