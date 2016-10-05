4e36e1d20a00b777a7f6518c2c513e3d10bd214e
Luke Keller <lukeekeller@gmail.com>
2016-10-05 16:01:10 -0400

    Create Overview and Intro Components
    Creates Overview and Intro Components. Sets up routing to Intro Component.
    
    * Create Overview Component
    * Create Intro Component
    * Modify routes to include Intro Component
    * Style Docs sidebar navigation

---
+++ src/app/components/policy-guide/docs/overview/overview.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overview',
+  template: require('./overview.template.html')
+})
+export class OverviewComponent {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
