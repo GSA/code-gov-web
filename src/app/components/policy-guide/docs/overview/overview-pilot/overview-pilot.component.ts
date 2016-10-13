5ec7eb837b1a599566194c1e8428839a6427a233
Luke Keller <lukeekeller@gmail.com>
2016-10-13 15:03:50 -0400

    Create OverviewPilot Component
    Adds an OverviewPilot Component as a placeholder for future content.
    
    * Create OverviewPilotComponent
    * Add necessary routes

---
+++ src/app/components/policy-guide/docs/overview/overview-pilot/overview-pilot.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overview-pilot',
+  template: require('./overview-pilot.template.html')
+})
+export class OverviewPilotComponent {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
