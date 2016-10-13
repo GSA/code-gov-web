320b8dc5295d51a9f09131dd581d6fc558ebf237
Luke Keller <lukeekeller@gmail.com>
2016-10-13 15:20:25 -0400

    Create OverviewTrackingProgress Component
    Creates a OverviewTrackingProgress Component as a placeholder for future content.
    
    * Create OverviewTrackingProgressComponent
    * Create app.components for consolidating lengthy import statements out of the app.modules and app.routes files

---
+++ src/app/app.components.ts
@@ -0,0 +1,18 @@
+export { AppComponent } from './components/app';
+export { APP_RESOLVER_PROVIDERS } from './app.resolver';
+export { DocsComponent } from './components/policy-guide/docs';
+export { HomeComponent } from './components/home';
+export {
+  IntroductionComponent
+} from './components/policy-guide/docs/overview/introduction';
+export { OverviewComponent } from './components/policy-guide/docs/overview';
+export {
+  OverviewInventoryComponent
+} from './components/policy-guide/docs/overview/overview-inventory';
+export {
+  OverviewPilotComponent
+} from './components/policy-guide/docs/overview/overview-pilot';
+export {
+  OverviewTrackingProgressComponent
+} from './components/policy-guide/docs/overview/overview-tracking-progress';
+export { PolicyGuideComponent } from './components/policy-guide';
---
+++ src/app/components/policy-guide/docs/overview/overview-tracking-progress/overview-tracking-progress.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overview-tracking-progress',
+  template: require('./overview-tracking-progress.template.html')
+})
+export class OverviewTrackingProgressComponent {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
