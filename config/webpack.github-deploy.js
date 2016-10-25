c43639223836dcce5371c9d8155cca6679bcf887
Luke Keller <lukeekeller@gmail.com>
2016-10-25 14:50:04 -0400

    GitHub Deploy Fix
    Fixes issues with the latest GitHub deploy by updating the Webpack syntax and plugins.
    
    * Update github-deploy configuration to use latest Webpack syntax
    * Add HtmlWebpackPlugin to ensure baseurl is appropriately assigned

--- config/webpack.github-deploy.js
+++ config/webpack.github-deploy.js
@@ -3,10 +3,12 @@
  */
 const helpers = require('./helpers');
 const ghDeploy = require('./github-deploy');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 const ghpages = require('gh-pages');
-const webpackConfig = ghDeploy.getWebpackConfigModule(); // the settings that are common to prod and dev
-
+const webpackConfig = ghDeploy.getWebpackConfigModule();
+const commonConfig = require('./webpack.common.js');
 
 /**
  * Webpack Constants
@@ -14,6 +16,7 @@ const webpackConfig = ghDeploy.getWebpackConfigModule(); // the settings that ar
 const GIT_REMOTE_NAME = 'origin';
 const COMMIT_MESSAGE = 'Updates';
 const GH_REPO_NAME = ghDeploy.getRepoName(GIT_REMOTE_NAME);
+const ENV = 'production';
 
 const METADATA = webpackMerge(webpackConfig.metadata, {
   /**
@@ -21,63 +24,64 @@ const METADATA = webpackMerge(webpackConfig.metadata, {
    * This also means all resource URIs (CSS/Images/JS) will have this prefix added by the browser
    * unless they are absolute (start with '/'). We will handle it via `output.publicPath`
    */
-  baseUrl:
-    '/site/presidential-innovation-fellows/' +
-    GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.metadata.baseUrl)
+  baseUrl: '/site/presidential-innovation-fellows/' + GH_REPO_NAME + '/',
+  ENV: ENV,
+  HMR: false
 });
 
-module.exports = webpackMerge(webpackConfig, {
-  /**
-   * Merged metadata from webpack.common.js for index.html
-   *
-   * See: (custom attribute)
-   */
-  metadata: METADATA,
 
+module.exports = function (env) {
 
-  output: {
-    /**
-     * The public path is set to the REPO name.
-     *
-     * `HtmlElementsPlugin` will add it to all resources url's created by it.
-     * `HtmlWebpackPlugin` will add it to all webpack bundels/chunks.
-     *
-     * In theory publicPath shouldn't be used since the browser should automatically prefix the
-     * `baseUrl` into all URLs, however this is not the case when the URL is absolute (start with /)
-     *
-     * It's important to prefix & suffix the repo name with a slash (/).
-     * Prefixing so every resource will be absolute (otherwise it will be url.com/repoName/repoName...
-     * Suffixing since chunks will not do it automatically (testes against about page)
-     */
-    publicPath:
-      '/site/presidential-innovation-fellows/' +
-      GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.output.publicPath)
-  },
+  return webpackMerge(webpackConfig({env: ENV}), {
 
-  plugins: [
-    function() {
-      this.plugin('done', function() {
-        console.log('Starting deployment to GitHub.');
+    output: {
+      publicPath: '/site/presidential-innovation-fellows/' + GH_REPO_NAME + '/'
+    },
 
-        const logger = function (msg) {
-          console.log(msg);
-        };
+    plugins: [
 
-        const options = {
-          logger: logger,
-          remote: GIT_REMOTE_NAME,
-          message: COMMIT_MESSAGE
-        };
+      new HtmlWebpackPlugin({
+        metadata: METADATA,
+        template: 'src/index.html',
+        chunksSortMode: 'dependency',
+        inject: 'head'
+      }),
 
-        ghpages.publish(webpackConfig.output.path, options, function(err) {
-          if (err) {
-            console.log('GitHub deployment done. STATUS: ERROR.');
-            throw err;
-          } else {
-            console.log('GitHub deployment done. STATUS: SUCCESS.');
-          }
-        });
-      })
-    }
-  ]
-});
+      new LoaderOptionsPlugin({
+        options: {
+          sassLoader: {
+            includePaths: [
+              require('bourbon').includePaths,
+              require('bourbon-neat').includePaths
+            ]
+          },
+        }
+      }),
+
+      function() {
+        this.plugin('done', function() {
+          console.log('Starting deployment to GitHub.');
+
+          const logger = function (msg) {
+            console.log(msg);
+          };
+
+          const options = {
+            logger: logger,
+            remote: GIT_REMOTE_NAME,
+            message: COMMIT_MESSAGE
+          };
+
+          ghpages.publish(webpackConfig({env: ENV}).output.path, options, function(err) {
+            if (err) {
+              console.log('GitHub deployment done. STATUS: ERROR.');
+              throw err;
+            } else {
+              console.log('GitHub deployment done. STATUS: SUCCESS.');
+            }
+          });
+        })
+      }
+    ]
+  });
+}
