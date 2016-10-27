b69f5f10d64c79c275c87bfa3f128bcfe1c3c0ee
Luke Keller <lukeekeller@gmail.com>
2016-10-27 19:44:56 -0400

    Scroll Top on Route Change

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,12 +1,8 @@
-/*
- * Angular 2 decorators and services
- */
 import { Component, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
 import { StateService } from '../../services/state';
-/*
- * App Component
- * Top Level Component
- */
+
 @Component({
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
@@ -15,13 +11,17 @@ import { StateService } from '../../services/state';
 })
 
 export class AppComponent {
-  constructor(public stateService: StateService) {}
-}
+  constructor(
+    private router: Router,
+    public stateService: StateService
+  ) {}
 
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      document.body.scrollTop = 0;
+    });
+  }
+}
