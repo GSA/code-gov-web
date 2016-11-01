3e500054c3e13e3ad93f80ad763ba3aeb3e06baf
Luke Keller <lukeekeller@gmail.com>
2016-11-01 11:33:06 -0400

    Create SEO Service
    Creates an SeoService to manage the modification of meta tags.
    
    * Create SeoService
    * Add SeoService to IntroductionComponent to demonstrate implementation

--- src/app/components/policy-guide/docs/overview/introduction/introduction.component.ts
+++ src/app/components/policy-guide/docs/overview/introduction/introduction.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SeoService } from '../../../../../services/seo';
 
 @Component({
   selector: 'introduction',
@@ -6,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class IntroductionComponent {
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor(private seoService: SeoService) {
+    seoService.setTitle('Policy Implementation Introduction', true);
+    seoService.setMetaDescription('Start here for an overview of the Federal Open Source Code Policy.');
+    seoService.setMetaRobots('Index, Follow');
   }
 }
