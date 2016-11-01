48df53e9c350b308d9bfcc32f6c12df0e18e4eec
Luke Keller <lukeekeller@gmail.com>
2016-11-01 16:48:12 -0400

    Responsive Homepage with Updated Design
    Add mobile styles to the Homepage and update the design.
    
    * Add mobile styles to Homepage
    * Update homepage design

--- src/app/utils/app-components/app-components.util.ts
+++ src/app/utils/app-components/app-components.util.ts
@@ -1,6 +1,7 @@
 export * from '../../components/app';
 export * from '../../components/four-oh-four';
 export * from '../../components/home';
+export * from '../../components/home/banner-art';
 export * from '../../components/modal';
 
 export * from '../../components/explore-code';
