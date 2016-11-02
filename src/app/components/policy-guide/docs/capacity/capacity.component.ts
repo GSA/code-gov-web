9e563872a9bbeab5a65eae9f947ed3a8b0572d75
Luke Keller <lukeekeller@gmail.com>
2016-11-02 15:47:28 -0400

    Add Mobile Menu to Policy Guide
    Adds a MobileService and ToggleMenuDirective for toggling the Policy Guide Mobile Menu.
    
    * Add Mobile Menu button to Policy Guide
    * Add mobile-friendly styles to Policy Guide
    * Create MobileService for toggling the Mobile Menu
    * Create ToggleMenuDirective for calling the MobileService

--- src/app/components/policy-guide/docs/capacity/capacity.component.ts
+++ src/app/components/policy-guide/docs/capacity/capacity.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { MobileService } from '../../../../services/mobile';
+
 @Component({
   selector: 'capacity',
   template: require('./capacity.template.html')
 })
 
 export class CapacityComponent {
+
+  constructor(private mobileService: MobileService) {
+    this.mobileService.hideMenu();
+  }
 }
