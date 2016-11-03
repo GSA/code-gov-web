4650e81dad10aaf7032b04532647cb795e6c9ed8
Luke Keller <lukeekeller@gmail.com>
2016-11-03 10:17:15 -0400

    Fix Safari Angular Issues and Add Cross-browser Styles
    Fixes an issue with Safari 5.1 that resulted in truncate pipe throwing an application error. Also adds styles for cross-browser friendliness.
    
    * Load polyfill library via cdn (temporary) to fix issues with Safari 5.1 and other older browsers
    * Covert terminal SVG to divs for improved cross-browser styles
    * Use SVG padding hack for logo to display logo properly across browsers
    * Set flag loading icon width and height to ensure stretching doesn't occur in older browsers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { EXPLORE_CODE_ROUTES } from './routes/explore-code';
 import { POLICY_GUIDE_ROUTES } from './routes/policy-guide';
 import { DataResolver } from './app.resolver';
 
-
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   ...EXPLORE_CODE_ROUTES,
