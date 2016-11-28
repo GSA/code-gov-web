69dba904edeee9dd7b185d3b0130e2e2ced43223
Luke Keller <lukeekeller@gmail.com>
2016-11-28 15:01:45 -0500

    Hide Mobile Menu on Route Change
    Hides the Mobile Menu on Route change.
    
    * Hide ToggleMenu on Route change via modified constructor
    * Add ToggleMenu tests
    * Force Angulartics2 Version

--- src/app/directives/toggle-menu/toggle-menu.directive.ts
+++ src/app/directives/toggle-menu/toggle-menu.directive.ts
@@ -1,4 +1,8 @@
 import { Directive, ElementRef, Output, Renderer } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
+
 import { MobileService } from '../../services/mobile';
 
 @Directive({
@@ -9,9 +13,28 @@ import { MobileService } from '../../services/mobile';
 })
 
 export class ToggleMenuDirective {
+  eventSub: Subscription;
   toggle: boolean;
-  constructor(private el: ElementRef, private mobileService: MobileService) {
+
+  constructor(
+    private el: ElementRef,
+    private mobileService: MobileService,
+    private router: Router
+  ) {
     this.toggle = JSON.parse(this.el.nativeElement.getAttribute('aria-pressed'));
+
+    this.eventSub = this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      this.mobileService.hideMenu();
+      this.toggle = false;
+      this.el.nativeElement.setAttribute('aria-pressed', this.toggle);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.eventSub) this.eventSub.unsubscribe();
   }
 
   onClick(event: any) {
