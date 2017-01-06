72c47ae59cb79027a1a36064df24d67d528bfcca
okamanda <olivier.kamanda@pif.gov>
2017-01-06 13:40:56 -0500

    remove compliance metadata schema folder

--- src/app/components/policy-guide/docs/compliance/compliance-metadata-schema/compliance-metadata-schema.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'metadata-schema',
-  template: require('./compliance-metadata-schema.template.html')
-})
-
-export class ComplianceMetadataSchemaComponent {
-}
