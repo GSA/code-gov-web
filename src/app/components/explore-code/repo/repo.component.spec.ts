69d97ef9aba65bfb92369b171f5c8f28c321d523
Olivier Kamanda <olivier.kamanda@pif.gov>
2017-05-30 15:18:02 -0400

    Ok ossconmaster (#244)
    * retire whats-new
    
    * rough draft for agency compliance dashboard
    
    * style and content tweaks to dashboard
    
    * don't include agencies that don't need to comply in the dash
    
    * a lil' decrufting
    
    * updating dashboard status
    
    * update federalist deployment script for production
    
    * updating compliance dashboard
    
    * updating compliance dashboard (NASA->green)
    
    * updating dashboard
    
    * updated list of agencies with OS or gov-wide reuse repos
    
    * added 2 DOD repos
    
    * updated repo template to use 'Contact Agency' and agency index template to use 'Government-Wide Reuse'
    
    * updated repo template for govwidereuse highlights; added new DOD repos;
    
    * add mailing list subscribe link/button
    
    * changed order of DOD repos
    
    * Added DOT, NRC logos and repos; added mailchimp bitly url
    
    * added pshtt to DHS repos
    
    * added new banner branding; changed width of branding box
    
    * remove external link notification for mailchimp
    
    * updating compliance dashboard
    
    * changed mailing list button to text
    
    * updated NRC - Adaptive Case Management
    
    * updating compliance dashboard
    
    * updating compliance dashboard
    
    * updating repository component test
    
    * updating compliance dashboard
    
    * update test suite to reflect new repo format from API (#243)
    
    * update test suite to reflect new repo format from API
    
    * adding updated repo.template.html
    
    * updated dashboard
    
    * fix DOT dashboard
    
    * update DOT dashboard
    
    * fixing repo template

--- src/app/components/explore-code/repo/repo.component.spec.ts
+++ src/app/components/explore-code/repo/repo.component.spec.ts
@@ -110,23 +110,29 @@ describe('RepoComponent', () => {
     let repo = createRepository({name: 'Another Fake repo name'});
     spyOn(reposService, 'getJsonFile').and.returnValue(Observable.of(repo));
     spyOn(seoService, 'setMetaDescription');
-
     fixture.detectChanges();
 
     expect(repoComponent.repo).toBeDefined();
     expect(seoService.setMetaDescription).toHaveBeenCalled();
   }));
 
+/******* Test repo.repository  ****/
 
-  it('should display repoUrl in template if repo.repoUrl property is set',
+
+  it('should display repository in template if repo.repository property is set',
     inject([AgencyService, ReposService, SeoService],
       (agencyService, reposService, seoService)  => {
     let agency = {id: 'VA', name: 'Department of Veterans Affairs'};
     spyOn(agencyService, 'getAgency').and.returnValue(agency);
-    const repoURL = 'http://www.github.com/foobar/';
-    let repo = createRepository(
-      {name: 'A Fake repo name to show repo',
-      repoURL: repoURL, homepage: 'http://code.gov/foobar/' });
+    const repository = 'http://www.github.com/repository/';
+    let repo = createRepository({
+      name: 'A Fake repo name to show repo',
+      repository: repository,
+      homepage: 'http://code.gov/homepage/',
+      openSourceProject: 1,
+      governmentWideReuseProject:0
+      
+    });
     spyOn(reposService, 'getJsonFile').and.returnValue(Observable.of(repo));
 
     fixture.detectChanges();
@@ -134,8 +140,13 @@ describe('RepoComponent', () => {
     let anchors = fixture.nativeElement.querySelectorAll('.usa-button');
     // console.log('Anchors: ', anchors);
 
-    // 2nd child anchor is the repoURL (first one is homepage)
-    expect(anchors[1].href).toBe(repoURL);
+    // 2nd child anchor is the repository (first one is homepage)
+    //console.log(anchors);
+    console.log("anchor 0"+ anchors[0].href);
+    //console.log("anchor 1"+ anchors[1].href);
+    
+    
+    expect(anchors[1].href).toBe(repository);
   }));
 
   it('should display repository name in template if repo.name property is defined',
@@ -202,26 +213,29 @@ describe('RepoComponent', () => {
     inject([AgencyService, ReposService, SeoService],
       (agencyService, reposService, seoService)  => {
         setupRepoPropertyTest(agencyService, reposService, seoService,
-          {homepage: undefined});
+
+          {homepage: undefined}
+          );
+
 
         fixture.detectChanges();
 
-        let parent = fixture.nativeElement.querySelector('.usa-unstyled-list');
-        expect(parent.children[0]).toBeUndefined();
+        let anchors = fixture.nativeElement.querySelectorAll('.usa-unstyled-list .usa-button');
+        expect(anchors[0]).toBeUndefined();
+        
       }
   ));
 
-
-  it('should NOT display repository homepage in template if repo.homepage property is null',
+  it('should NOT display repository homepage in template if repo.homepage property is null ',
     inject([AgencyService, ReposService, SeoService],
       (agencyService, reposService, seoService)  => {
         setupRepoPropertyTest(agencyService, reposService, seoService,
           {homepage: null});
 
         fixture.detectChanges();
-
-        let parent = fixture.nativeElement.querySelector('.usa-unstyled-list');
-        expect(parent.children[0]).toBeUndefined();
+        
+        let anchors = fixture.nativeElement.querySelectorAll('.usa-unstyled-list .usa-button');
+        expect(anchors[0]).toBeUndefined();
       }
   ));
 
@@ -229,59 +243,18 @@ describe('RepoComponent', () => {
     inject([AgencyService, ReposService, SeoService],
       (agencyService, reposService, seoService)  => {
         setupRepoPropertyTest(agencyService, reposService, seoService,
-          {homepage: 'http://code.gov/'});
+
+          {homepage: 'http://code.gov/', openSourceProject: 1});
+
 
         fixture.detectChanges();
 
-        let parent = fixture.nativeElement.querySelector('.usa-unstyled-list');
+     let parent = fixture.nativeElement.querySelectorAll('.usa-unstyled-list .usa-button');
+     
+     console.log("homepage is defined test: "+parent);
+  
+      expect(parent).toBeDefined();
 
-        expect(parent.children[0].children[0]).toBeDefined();
-      }
-  ));
-
-/******* Test repo.repoUrl  ****/
-
-  it('should NOT display repository url in template if repo.repoURL property is undefined',
-    inject([AgencyService, ReposService, SeoService],
-      (agencyService, reposService, seoService)  => {
-        setupRepoPropertyTest(agencyService, reposService, seoService,
-          {repoURL: undefined, homepage: 'http://foo.bar'});
-
-        fixture.detectChanges();
-
-        let parent = fixture.nativeElement.querySelector('.usa-unstyled-list');
-
-        expect(parent.children[1]).toBeUndefined();
-      }
-  ));
-
-
-  it('should NOT display repository Url in template if repo.repoURL property is null',
-    inject([AgencyService, ReposService, SeoService],
-      (agencyService, reposService, seoService)  => {
-        setupRepoPropertyTest(agencyService, reposService, seoService,
-          {repoURL: null, homepage: 'http://foo.bar'});
-
-        fixture.detectChanges();
-
-        let parent = fixture.nativeElement.querySelector('.usa-unstyled-list');
-        // console.log('PARENT: ', parent);
-        expect(parent.children[1]).toBeUndefined();
-      }
-  ));
-
-  it('should display repository repoUrl in template if repo.repoURL property is defined',
-    inject([AgencyService, ReposService, SeoService],
-      (agencyService, reposService, seoService)  => {
-        setupRepoPropertyTest(agencyService, reposService, seoService,
-          {repoURL: 'http://code.gov/repo', homepage: undefined});
-
-        fixture.detectChanges();
-
-        let parent = fixture.nativeElement.querySelector('.usa-unstyled-list');
-        // console.log('PARENT: ', parent);
-
-        expect(parent.children[0]).toBeDefined();
       }
   ));
 
@@ -304,7 +277,11 @@ interface RepoProps {
   name?: string;
   description?: string;
   homepage?: string;
-  repoURL?: string;
+
+  repository?: string;
+  openSourceProject?: number;
+  governmentWideReuseProject?: number;
+  
 }
 
 /**
@@ -316,7 +293,8 @@ export function createRepository(repoProps: RepoProps) {
       repos: [
         { repoID : id, name: repoProps.name, description: repoProps.description,
           codeLanguage: [{language: 'JavaScript'}], agency: 'VA',
-          homepage: repoProps.homepage, repoURL: repoProps.repoURL }
+          homepage: repoProps.homepage, repository: repoProps.repository,
+          openSourceProject: repoProps.openSourceProject, governmentWideReuseProject: repoProps.governmentWideReuseProject}
         ]
     };
 }
--- src/app/components/policy-guide/docs/compliance/compliance-dashboard/compliance-dashboard.component.ts
+++ src/app/components/policy-guide/docs/compliance/compliance-dashboard/compliance-dashboard.component.ts
@@ -1,4 +1,10 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AgencyService, Agency } from '../../../../../services/agency';
+import { StatusService } from '../../../../../services/status';
+
+import { Subscription } from 'rxjs/Subscription';
 import { SeoService } from '../../../../../services/seo';
 
 
@@ -7,10 +13,99 @@ import { SeoService } from '../../../../../services/seo';
   template: require('./compliance-dashboard.template.html')
 })
 
-export class ComplianceDashboardComponent {
-constructor(private seoService: SeoService) {
-    seoService.setTitle('Policy Compliance - Dashboard', true);
-    seoService.setMetaDescription('Dashboard illustrating federal agency compliance with the 90-day and 120-day policy requirements.');
-    seoService.setMetaRobots('Index, Follow');
+export class ComplianceDashboardComponent implements OnInit, OnDestroy {
+  agencyIds[];
+  public statuses[];
+  public updated;
+  private statusesSub: Subscription;
+
+  constructor(
+    private agencyService: AgencyService,
+    private statusService: StatusService
+    ) {}
+
+  ngOnInit() {
+    this.getAgencyIds();
+    this.getStatuses();
   }
+
+  ngOnDestroy(){
+  	if (this.statusesSub) this.statusesSub.unsubscribe();
+  }
+
+
+  getAgencyIds(){
+    this.agencyIds = [];
+    var agencies = this.agencyService.getAgencies();
+    for(let agency of agencies){
+      this.agencyIds.push(agency.id);
+    }
+  }
+
+  getStatuses() {
+    var agency;
+    var requirements
+    var rValue;
+    var requirementStatus;
+    var overallStatus;
+    var codePath;
+
+    this.statusesSub = this.statusService.getJsonFile().
+      subscribe((result) => {
+        if (result) {
+
+          for (let status in result.statuses) {
+ 			      
+             //if agencyWidePolicy is null in report.json it means the agency doesn't have to comply, 
+             //so don't include it in the dash.
+             //TODO: should make this more explicit in the API, 
+            if(result.statuses[status].requirements["agencyWidePolicy"] != null){
+              
+              // TODO: pull from AgencyService, which currently only provides agencies that have code included on the site.
+
+              requirements = new Array();
+              for(let requirement in result.statuses[status].requirements){
+               
+                rValue = result.statuses[status].requirements[requirement];
+                if(rValue <1){
+                  if(rValue > 0){
+                    requirementStatus = "partial";
+                  }
+                  else {
+                    requirementStatus = "noncompliant";
+                  }
+                }
+                else{
+                  requirementStatus = "compliant";
+                }
+
+                if(requirement != "overallCompliance"){
+                  requirements.push({"text": requirement, "status": requirementStatus});
+                }
+                else{
+                 overallStatus = requirementStatus;
+                }
+
+              }
+
+              if(this.agencyIds.find(function(x){return x==status;})){
+                codePath = "/explore-code/agencies/" + status;
+              }
+              else{
+                codePath = null;
+              }
+
+              agency = { "id" : status, "name" : result.statuses[status].metadata.agency.name, "overall" : overallStatus, "codePath": codePath }; 
+            	this.statuses.push({"id": status, "agency": agency, "requirements": requirements});
+              this.updated = result.timestamp;
+
+            }
+          }
+        } else {
+          console.log('Error.');
+        }
+    });
+
+  }
+
 }
