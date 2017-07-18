89e198eb9ef0bb4b9eb2a48307e3be8019325dd0
Olivier Kamanda <olivier.kamanda@pif.gov>
2017-07-18 11:53:12 -0400

    Revert "Implement style guide [WIP] (#271)" (#272)
    This reverts commit 3811832838382d8625b8c9a04bda4137b2b7409f.

--- src/app/components/policy-guide/policy/policy.component.ts
+++ src/app/components/policy-guide/policy/policy.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { MobileService } from '../../../services/mobile';
 
 @Component({
   selector: 'policy',
@@ -6,6 +9,21 @@ import { Component, ViewEncapsulation } from '@angular/core';
   template: require('./policy.template.html'),
   encapsulation: ViewEncapsulation.None
 })
-export class PolicyComponent {
+export class PolicyComponent implements OnDestroy {
+  menuActive: boolean;
+  activeMenuSub: Subscription;
 
+  constructor(private mobileService: MobileService) {
+    this.menuActive = false;
+
+    this.activeMenuSub = mobileService.activeMobileMenu$.subscribe(
+      menuStatus => {
+        this.menuActive = menuStatus;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.activeMenuSub) this.activeMenuSub.unsubscribe();
+  }
 }
