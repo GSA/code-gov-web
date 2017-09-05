7120eda817d17a19b7be24c9c2f3187ee09e65ff
Zachary Kuhn <zachary.kuhn@gmail.com>
2017-09-05 09:41:07 -0400

    Add JSDoc to many of the components.

--- src/app/components/repos-search/repos-search.component.ts
+++ src/app/components/repos-search/repos-search.component.ts
@@ -18,6 +18,10 @@ import { Router } from '@angular/router';
 import { SearchService } from '../../services/search';
 import { TermService } from '../../services/term';
 
+/**
+ * Class representing a search component for repositories.
+ */
+
 @Component({
   selector: 'repos-search',
   template: require('./repos-search.template.html'),
@@ -34,6 +38,15 @@ export class ReposSearchComponent {
   public autocompleteVisible: boolean = false;
   public queryInputValue: string = '';
 
+  /**
+   * Constructs a ReposSearchComponent.
+   *
+   * @constructor
+   * @param {FormBuilder} formBuilder - A service for building forms in angular
+   * @param {SearchService} searchService - A service for searching repositories
+   * @param {TermService} termService - A service for finding matching terms
+   * @param {Router} router - The application's URL router
+   */
   constructor(
     public formBuilder: FormBuilder,
     private searchService: SearchService,
@@ -41,10 +54,18 @@ export class ReposSearchComponent {
     private router: Router,
   ) {}
 
+  /**
+   * Whether the user has entered any search terms.
+   *
+   * @return {boolean} true if the search input has values
+   */
   hasQuery(): boolean {
     return this.queryInputValue.length > 0;
   }
 
+  /**
+   * On initialization of the ReposSearchComponent
+   */
   ngOnInit() {
     this.searchForm = this.formBuilder.group({
       query: [''],
@@ -54,12 +75,21 @@ export class ReposSearchComponent {
     this.queryInputValue = this.queryValue;
   }
 
+  /**
+   * After everything has been added to the DOM
+   */
   ngAfterViewInit() {
     if (this.autofocus) {
       this.queryElement.nativeElement.focus();
     }
   }
 
+  /**
+   * Execute a search on every keypress. Escape and Enter blur the input.
+   *
+   * @param {Event} event - Keyboard event from user pressing a key
+   * @return {void}
+   */
   onKey(event: any) {
     this.queryInputValue = event.target.value;
     this.termService.search(this.queryInputValue);
@@ -69,26 +99,50 @@ export class ReposSearchComponent {
     }
   }
 
+  /**
+   * When form is submitted, go to search results page.
+   *
+   * @param {any} form - The form being submitted
+   * @return {void}
+   */
   onSubmit(form: any): void {
     this.search(form.query);
   }
 
+  /**
+   * Navigate to the search results page.
+   *
+   * @param {string} query - The search terms we are searching
+   * @return {void}
+   */
   search(query: string) {
     this.router.navigateByUrl('/search?q=' + query);
   }
 
+  /**
+   * Show the autocomplete dropdown.
+   */
   showAutocomplete() {
     this.autocompleteVisible = true;
   }
 
+  /**
+   * Hide the autocomplete dropdown.
+   */
   hideAutocomplete() {
     this.autocompleteVisible = false;
   }
 
-  onBlurHandler(e) {
-
+  /**
+   * When a click happens on the page, if it is outside the form,
+   * hide the autocomplete.
+   *
+   * @param {Event} event - The mouse click event.
+   * @return {void}
+   */
+  onBlurHandler(event: any) {
     const inputDoesNotHaveFocus = window.document.activeElement !== this.queryElement.nativeElement;
-    const clickIsOutsideForm = !this.searchFormElement.nativeElement.contains(e.target);
+    const clickIsOutsideForm = !this.searchFormElement.nativeElement.contains(event.target);
 
     if (inputDoesNotHaveFocus && clickIsOutsideForm) {
 
