834d62219b4359fc30832e461de5e6a0766e1a6b
Zachary Kuhn <zachary.kuhn@gmail.com>
2017-12-15 10:36:49 -0500

    Initial work on Help Wanted.

---
+++ src/app/components/help-wanted/help-wanted.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'help-wanted',
+  styles: [require('./help-wanted.styles.scss')],
+  template: require('./help-wanted.template.html')
+})
+
+export class HelpWantedComponent {
+  
+}
