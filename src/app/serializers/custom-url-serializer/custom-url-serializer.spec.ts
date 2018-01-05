6a6a8f19425cd1dd6f62f1a400dad3c3801d782a
Froilan Irizarry <froilan@froilanirizarry.me>
2018-01-05 15:45:13 -0500

    Fixed PR merge mistake. - I merged a PR to master without looking at the build outputs. We have been having some errors with CircleCI and CodeClimate so I assumed it was the same "Oh they are not working again". When I merged the build ran correctly and some linter errors showed up.

--- src/app/serializers/custom-url-serializer/custom-url-serializer.spec.ts
+++ src/app/serializers/custom-url-serializer/custom-url-serializer.spec.ts
@@ -6,7 +6,7 @@ import { CustomUrlSerializer } from './custom-url-serializer';
 
 @Component({
     selector: '',
-    template: `<a routerLink="{{url}}">test link</a>`
+    template: `<a routerLink='{{url}}'>test link</a>`
 })
 class TestRoutingWithFullUrlComponent {
     private url: string;
@@ -21,20 +21,20 @@ class TestRoutingWithFullUrlComponent {
  * Unit tests for CustomUrlSerializer including tests for 3 things:
  * 1) CustomUrlSerializer.parse():
  * The parse function converts a relative url into a UrlTree.
- * 
+ *
  * 2) CustomUrlSerializer.serialize():
  * The serialize function converts a UrlTree to a relative url string.
- * 
+ *
  * 3) Usage of routerLink:
- * routerLink, when used in a template, sets an href attribute on an 
+ * routerLink, when used in a template, sets an href attribute on an
  * a tag based on an expression between double curly braces
  */
 describe('CustomUrlSerializer', () => {
-  
+
   // The url serializer created for this app that correctly encodes parentheses
   let customUrlSerializer: CustomUrlSerializer;
-  
-  // The url serializer that comes with Angular 2 and doesn't encode parentheses 
+
+  // The url serializer that comes with Angular 2 and doesn't encode parentheses
   let defaultUrlSerializer: DefaultUrlSerializer;
 
   beforeEach(() => {
@@ -46,94 +46,94 @@ describe('CustomUrlSerializer', () => {
 
     describe('when a regular url without need for any special encoding/escaping is given', () => {
       it("the serializer's parsing method does not escape anything ", () => {
-        
-        let url: string = "/explore-code/agencies/DOJ"
-        
-        let expected_url_segments: Object[] = [
-          {"path":"explore-code","parameters":{}},
-          {"path":"agencies","parameters":{}},
-          {"path":"DOJ","parameters":{}}
+
+        let url: string = '/explore-code/agencies/DOJ';
+
+        let expectedUrlSegments: Object[] = [
+          {'path': 'explore-code', 'parameters': {}},
+          {'path': 'agencies', 'parameters': {}},
+          {'path': 'DOJ', 'parameters': {}}
         ];
-        
-        let actual_url_segments = customUrlSerializer.parse(url).root.children.primary.segments;
-        
+
+        let actualUrlSegments = customUrlSerializer.parse(url).root.children.primary.segments;
+
         // Compare the value of the url segments, not whether they're techncially the same object in memory
-        expect(JSON.stringify(actual_url_segments)).toEqual(JSON.stringify(expected_url_segments));
+        expect(JSON.stringify(actualUrlSegments)).toEqual(JSON.stringify(expectedUrlSegments));
 
       });
     });
 
     describe('when a url with escaped parentheses is used', () => {
       it('the serializer parsed and unescaped correctly', () => {
-        
-        let url : string = "/explore-code/agencies/DOJ/repos/Better%20View%20Pane%20Args%20%28Drupal%20module%29";
-        
-        let expected : Array<Object> = [
-          {path:"explore-code",parameters:{}},
-          {path:"agencies",parameters:{}},
-          {path:"DOJ",parameters:{}},
-          {path:"repos",parameters:{}},
-          {path:"Better View Pane Args (Drupal module)",parameters:{}}
+
+        let url: string = '/explore-code/agencies/DOJ/repos/Better%20View%20Pane%20Args%20%28Drupal%20module%29';
+
+        let expected: Array<Object> = [
+          {path: 'explore-code', parameters: {}},
+          {path: 'agencies', parameters: {}},
+          {path: 'DOJ', parameters: {}},
+          {path: 'repos', parameters: {}},
+          {path: 'Better View Pane Args (Drupal module)', parameters: {}}
         ];
-        
+
         let actual = customUrlSerializer.parse(url).root.children.primary.segments;
 
         expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected));
-        
+
       });
     });
 
     describe('when a url with unescaped spaces and parentheses is used', () => {
       it('the serializer parsed correctly', () => {
-        
-        let url : string = "/explore-code/agencies/DOJ/repos/Better View Pane Args (Drupal module)";
-        
-        let expected : Array<Object> = [
-          {path:"explore-code",parameters:{}},
-          {path:"agencies",parameters:{}},
-          {path:"DOJ",parameters:{}},
-          {path:"repos",parameters:{}},
-          {path:"Better View Pane Args (Drupal module)",parameters:{}}
+
+        let url: string = '/explore-code/agencies/DOJ/repos/Better View Pane Args (Drupal module)';
+
+        let expected: Array<Object> = [
+          {path: 'explore-code', parameters: {}},
+          {path: 'agencies', parameters: {}},
+          {path: 'DOJ', parameters: {}},
+          {path: 'repos', parameters: {}},
+          {path: 'Better View Pane Args (Drupal module)', parameters: {}}
         ];
-        
+
         let actual = customUrlSerializer.parse(url).root.children.primary.segments;
 
         expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected));
-        
+
       });
     });
 
   });
-  
-  
+
   describe('serialize', () => {
-    describe("when a normal url tree without parentheses", () => {
-      it("returns the correct url", () => {
-        
-        let original_url : string = "/explore-code/agencies/NASA";
-        
-        let urlTree : UrlTree = defaultUrlSerializer.parse(original_url);
-        
-        let actual_url : string = customUrlSerializer.serialize(urlTree);
-        
+    describe('when a normal url tree without parentheses', () => {
+      it('returns the correct url', () => {
+
+        let originalUrl: string = '/explore-code/agencies/NASA';
+
+        let urlTree: UrlTree = defaultUrlSerializer.parse(originalUrl);
+
+        let actualUrl: string = customUrlSerializer.serialize(urlTree);
+
         /* we're basically checking if the customer url serializer
          * gives the same results as the default url serializer
          * when there are no parentheses present
          */
         expect(actual_url).toEqual(original_url);
-        
+
       });
     });
-    
-    describe("when a urlTree represnting a url with parenthese is given", () => {
-      it("the Custom Url Serializer should correctly serialize with escaping", () => {
-        
-        let original_url : string = "/explore-code/agencies/NASA/repos/Abaqus%20User%20Subroutine%20Verification%20%28abaverify%29";
 
-        let urlTree : UrlTree = defaultUrlSerializer.parse(original_url);
+    describe('when a urlTree represnting a url with parenthese is given', () => {
+      it('the Custom Url Serializer should correctly serialize with escaping', () => {
 
-        let actual_url : string = customUrlSerializer.serialize(urlTree);
-        
+        let originalUrl: string = '/explore-code/agencies/NASA/' +
+          'repos/Abaqus%20User%20Subroutine%20Verification%20%28abaverify%29';
+
+        let urlTree: UrlTree = defaultUrlSerializer.parse(originalUrl);
+
+        let actualUrl: string = customUrlSerializer.serialize(urlTree);
+
         /*
           We're basically checking to see if the serialize method correctly
           returns the reverse of the parse method.  We should note that we
@@ -144,14 +144,14 @@ describe('CustomUrlSerializer', () => {
       });
     });
 
-  });  
-  
+  });
+
 
   describe('Custom Url Serializer integrated in a template', () => {
-    
+
       let fixture: ComponentFixture<TestRoutingWithFullUrlComponent>;
       let testComponent: TestRoutingWithFullUrlComponent;
-      
+
       beforeEach(() => {
           TestBed.configureTestingModule({
               imports: [
@@ -159,10 +159,10 @@ describe('CustomUrlSerializer', () => {
               ],
               declarations: [TestRoutingWithFullUrlComponent],
               providers: [
-                { provide: APP_BASE_HREF, useValue: "/" },
-                
+                { provide: APP_BASE_HREF, useValue: '/' },
+
                 { provide: LocationStrategy, useClass: HashLocationStrategy },
-                
+
                 // CustomUrlSerializer replaces UrlSerializer like in app.module.ts
                 { provide: UrlSerializer, useClass: CustomUrlSerializer }
               ],
@@ -172,18 +172,21 @@ describe('CustomUrlSerializer', () => {
       });
 
       it('should set correct href when use unescaped url', () => {
-        
-          let original_url : string = "/explore-code/agencies/NASA/repos/Acoustic Propagation and Emulation Toolset (APET)";
-          testComponent.setUrl(original_url); 
+
+          let originalUrl: string = '/explore-code/agencies/NASA/repos/Acoustic' +
+            'Propagation and Emulation Toolset (APET)';
+
+          testComponent.setUrl(original_url);
 
           fixture.detectChanges();
 
           let actualHref = fixture.nativeElement.querySelector('a').href;
 
-          let expectedUrl = window.location.href + "#" + "/explore-code/agencies/NASA/repos/Acoustic%20Propagation%20and%20Emulation%20Toolset%20%28APET%29";
+          let expectedUrl = window.location.href + '#' +
+            '/explore-code/agencies/NASA/repos/Acoustic%20Propagation%20and%20Emulation%20Toolset%20%28APET%29';
 
           expect(actualHref).toEqual(expectedUrl);
       });
   });
-  
+
 });
--- src/app/serializers/custom-url-serializer/custom-url-serializer.ts
+++ src/app/serializers/custom-url-serializer/custom-url-serializer.ts
@@ -5,23 +5,23 @@ export class CustomUrlSerializer implements UrlSerializer {
     private _defaultUrlSerializer: DefaultUrlSerializer = new DefaultUrlSerializer();
 
     parse(url: string): UrlTree {
-        
+
         /*
             We, counter-intuitively, replace parentheses with their
             escaped equivalent because the DefaultUrlSerializer
             incorrectly stops parsing when it hits a parentheses.
-            
-            It does however correctly parse urls with 
+
+            It does however correctly parse urls with
             escaped parentheses.
         */
         url = url.replace(/\(/g, '%28').replace(/\)/g, '%29');
-        
+
         return this._defaultUrlSerializer.parse(url);
 
     }
 
     serialize(tree: UrlTree): string {
-        
+
         return this._defaultUrlSerializer.serialize(tree).replace(/\(/g, '%28').replace(/\)/g, '%29');
 
     }
