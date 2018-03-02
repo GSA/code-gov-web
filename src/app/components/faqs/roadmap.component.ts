526c8174eb24555b590256cf58980fb1e6374cf3
jcastle <joseph.castle@gsa.gov>
2018-03-02 16:35:43 -0500

    added component folder with files copied from Toadmap as a start

---
+++ src/app/components/faqs/roadmap.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * Class representing faqs.
+ */
+
+@Component({
+  selector: 'faqs',
+  styles: [require('./faqs.styles.scss')],
+  template: require('./faqs.template.html'),
+})
+
+export class faqsComponent {
+
+}
